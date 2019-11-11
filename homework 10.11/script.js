fbe38efbf4e58a59b8307778e7de5e22b38ad2aa
unknown <doriel494@gmail.com>
2019-11-11 20:11:55 +0200

    fixed code

--- homework 10.11/script.js
+++ homework 10.11/script.js
@@ -2,19 +2,22 @@ function holoCube(){
     let numOfDig= document.getElementById('Id').value
     let n=numOfDig.length;
     if(n==9){
-        let n=1;
-        while(numOfDig[n-1]>=0 && numOfDig[n-1]<=9){
-            n++
+    for(let i=0; i<n; i++){
+        if(!(numOfDig[i]>=0 && numOfDig[i]<=9)) {
+         console.log('false');
+            
         }
-        
-        return'true';  
     }
+
+    console.log('true');
+}
     else{
-        return 'false';
-    }  
+        console.log('false');
+        
+    }
 }
 // cheack if the id is ok
 
-function primary(num){
-if(num)
-}
+// function primary(num){
+
+// 
