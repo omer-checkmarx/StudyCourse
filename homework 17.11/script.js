686631c0f58786c84e8cc7d043c59c93fbaaaecd
unknown <doriel494@gmail.com>
2019-11-23 22:31:58 +0200

    targil 6

--- homework 17.11/script.js
+++ homework 17.11/script.js
@@ -34,8 +34,51 @@ bubbleSort([10,245,2315,545]);
 
 
 
-// let a=0;
-// let b=0;
+
+debugger
+function singleNum(arr){
+    let temp=0;
+    for(i = 0; i < arr.length; i++){
+        for(j = i+1; j < arr.length; j++){
+            if(arr[i]==arr[j]){
+              temp=arr[j]  ;
+              arr[j]=arr[arr.length-1];
+              arr[arr.length-1]=temp;
+              arr.pop();
+            //   if(i>0){
+            //   i--;
+            //   }
+              break;
+            }
+        }
+    }
+    console.log(arr);
+    // return arr;
+}
+singleNum([2,2,5,7,5,7]);
+// debugger
+function useOfTargil3(arr2 , arr3){
+    let arr2new=singleNum(arr2);
+    let arr3new=singleNum(arr3);
+    let Narray=[0];
+    for(i = 0; i < arr2new.length; i++){
+        for(j = 0; i < arr3new.length; j++){
+            if(arr2new[i]==arr3new[j]){
+                Narray.push(arr2new[i]);
+                break;
+            }
+        }
+    }
+    console.log(Narray); 
+    // return Narray;  
+}
+useOfTargil3([3,5,7,1,3,9] , [2,2,5,7,5,7]);
+
+
+
+
+
+
 function primeNumber(num){
     for(let i=2; i<=(num-1); i++) {
         if(num%i==0){
@@ -46,12 +89,7 @@ function primeNumber(num){
     console.log('prime');  
     return true;
 }
-  
-
 // primeNumber(10);
-
-
-
 // debugger
 let primal=0;
 function   Targil4(rishoniMat){
@@ -60,13 +98,94 @@ function   Targil4(rishoniMat){
             if(primeNumber(rishoniMat[m][n])){
                 primal++;
             }
-                
-
-        }   
-                
+        }            
     }
     console.log(primal);
-    
 }
 
 Targil4([[10,11,12,13,14],[20,21,22,23,24],[30,31,32,33,34]]);
+
+
+
+
+let count =0;
+function Targil5(mat){
+    for(i = 0; i < mat.length;i++){
+        for(j = 0; j < mat[i].length; j++){
+            if(mat[i][j] >= 'A' && mat[i][j] <= 'Z'){
+                count++
+            }
+        }
+    }
+    console.log(count);
+    
+}
+Targil5([['d','b','R','n','u'],['S','X','a','w','y'], ['v','u','Q','p']]);
+
+
+
+
+function islandAmount(mat){
+    let counter = 0;
+    for( i = 0; i <mat.length; i++){
+        for(j = 0; j < mat[i].length; j++){
+            if(mat[i][j]==1){
+                if(isIsland(mat,i, j , mat.length)){
+                    counter++;
+                }
+            }
+        }
+    }
+    return counter;
+}
+
+
+
+
+function isIsland(mat, i ,j, lenghtOfMatrix){
+    if( (j == 0 || mat[i][j-1]==0) &&
+        ((i == 0 || j ==0) || mat[i-1][j-1]==0) &&
+        (i==0 || mat[i-1][j]==0) &&
+        ((i == 0 || j == lenghtOfMatrix-1) || mat[i-1][j+1]==0) &&
+        (j == lenghtOfMatrix-1 || mat[i][j+1]==0) &&
+        (( i == lenghtOfMatrix-1 || j == lenghtOfMatrix-1) || mat[i+1][j+1]==0) &&
+        (i== lenghtOfMatrix-1 || mat[i+1][j]==0) &&
+        ((i == lenghtOfMatrix-1 || j == 0) || mat[i+1][j-1]==0)){
+            return true;
+    }
+    return false;
+}
+
+console.log(islandAmount([[0,0,0,1], 
+            [0,1,0,1],
+            [0,0,0,0],
+            [0,0,1,0]]));
+
+
+
+
+function check(array){
+    for(i = 0; i < array.length; i++){
+        for(j = i+1; j < array.length; j++){
+            if(array[i]==array[j]){
+                return notgood;
+            }
+        }
+    }
+    return good
+}
+
+
+
+
+
+function allOfNums(mat){
+    for(i = 0; i <mat.length; i++){
+        let cotain1=check(mat[i]);
+    }
+}
+// פונקציה שבודקת אם יש במערך כפל מספרים מ 1 עד 9 ואם כן היא מחזירה "לא טוב" ואם לא אז מחזירה "טוב".ו
+// היא חוזרת לתוך משתנה בטנקציה העיקריצ שתפקידה לבדוק  אם חזרו 9 פעמים "טוב" 
+// ואם באמת חזרו  פעמים 9 "טוב" אז ההמטריצה תקינה ואם לא אז היא לא תקינה
+
+
